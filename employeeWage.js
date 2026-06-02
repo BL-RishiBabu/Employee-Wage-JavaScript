b5caadb53e72a38090eda4007dac4e9791ab65c9
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-06-02 14:12:27 +0530

    Refactor getWorkingHours function for employee wage calculation

--- employeeWage.js
+++ employeeWage.js
@@ -5,19 +5,21 @@ const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const IS_ABSENT = 0;
 
-let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3;
-
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs = IS_ABSENT;
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return IS_ABSENT;
+    }
 }
+
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+
+empHrs = getWorkingHours(empCheck);
 
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage: " + empWage);
