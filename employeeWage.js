fee470a2abb68f026d50362c82fc8b03da74b431
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-06-02 14:18:17 +0530

    Refactor employee wage calculation to iterate over working days and accumulate hours

--- employeeWage.js
+++ employeeWage.js
@@ -4,6 +4,7 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const IS_ABSENT = 0;
+const MAX_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -17,9 +18,10 @@ function getWorkingHours(empCheck) {
 }
 
 let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-
-empHrs = getWorkingHours(empCheck);
+for (let day = 0; day < MAX_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
 
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage: " + empWage);
+console.log("Total Hours Worked: " + empHrs + " Total Employee Wage: " + empWage);
